c7f4d82b682ad967855fb331d0fdd322c6848085
Hakobyan98 <57825989+Hakobyan98@users.noreply.github.com>
2019-11-16 20:05:21 +0400

    Update Lesson1.kt

--- src/Lesson1.kt
+++ src/Lesson1.kt
@@ -2,57 +2,66 @@ import java.util.*
 
 fun main() {
     var flag: Boolean = true
-    loop@ while (flag) {
-        val random = (0..100).random()
+    loop1@ while (flag) {
+        val random = (1..100).random()
         println("Please select the dificulty level")
         println("1 - Easy")
         println("2 - Medium")
         println("3 - Hard")
 
         val reader = Scanner(System.`in`)
-        var input: Int = reader.nextInt()
-        var i : Int
+        val input: Int = reader.nextInt()
+        var numberOfSteps: Int
         when (input) {
-            1 -> i = 7
-            2 -> i = 5
-            3 -> i = 3
+            1 -> numberOfSteps = 7
+            2 -> numberOfSteps = 5
+            3 -> numberOfSteps = 3
             else -> {
                 println("please select one of the options")
-                continue@loop
+                continue@loop1
             }
         }
 
         println("Select a number from 1-100")
-        println("Note: you have $i steps")
+        println("Note: you have $numberOfSteps steps")
         var myAnswer: Int
-        while (i > 1) {
-            myAnswer = reader.nextInt()
-            i--;
-            if (myAnswer > random)
-                println("take lower")
-            if (myAnswer < random)
-                println("take higher")
-            if (myAnswer == random) {
-                println("You won!!! Congratulations...")
-                return;
+        while (numberOfSteps > 0) {
+            try {
+                myAnswer = reader.nextInt()
+                if(myAnswer < 1 || myAnswer > 100){
+                    throw NumberFormatException()
+                }
+            } catch (e: Exception) {
+                println("Please select a number from 1-100")
+                continue
             }
-            println("Note: you have $i steps")
-        }
 
-        myAnswer = reader.nextInt()
-        if (myAnswer != random) {
-            println("NOOOOOOOO  My number was $random")
-            println("1 - Restart    2 - End")
-            myAnswer = reader.nextInt()
-            when (myAnswer) {
-                1 -> continue@loop
-                2 -> flag = false
-                else -> println("Please select one of the options")
+            numberOfSteps--
+            if (numberOfSteps == 0) {
+                if (myAnswer != random) {
+                    println("NOOOOOOOO  My number was $random")
+                    println("1 - Restart    2 - End")
+                    myAnswer = reader.nextInt()
+                    when (myAnswer) {
+                        1 -> continue@loop1
+                        2 -> flag = false
+                        else -> println("Please select one of the options")
+                    }
+                } else {
+                    println("You won!!! Congratulations...")
+                    flag = false
+                }
+            } else {
+                if (myAnswer > random)
+                    println("take lower")
+                if (myAnswer < random)
+                    println("take higher")
+                if (myAnswer == random) {
+                    println("You won!!! Congratulations...")
+                    return;
+                }
+                println("Note: you have $numberOfSteps steps")
             }
-        } else {
-            println("You won!!! Congratulations...")
-            flag = false
         }
-
     }
 }
